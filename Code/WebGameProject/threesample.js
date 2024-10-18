e98b787f2f87f5d1570d43c8f6bfb2fc9ecf49c1
EthanBarrett <145779498+EthanBarrett@users.noreply.github.com>
2024-10-18 16:36:24 +0100

    Update threesample.js

--- Code/WebGameProject/threesample.js
+++ Code/WebGameProject/threesample.js
@@ -19,14 +19,14 @@ const material1 = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
 const Body = new THREE.Mesh( geometry1, material1 );
 Body.position.y = -5;
 //rightarm
-const geometry2 = new THREE.BoxGeometry( 3, 7, 2 ); 
+const geometry2 = new THREE.BoxGeometry( 3, 7, 2.5 ); 
 const material2 = new THREE.MeshBasicMaterial( {color: 0x00fff0} ); 
 const RightArm = new THREE.Mesh( geometry2, material2 ); 
 RightArm.position.x = 7;
 RightArm.position.y = -5;
 RightArm.rotation.z = 0.5;
 //leftarm
-const geometry3 = new THREE.BoxGeometry( 3, 7, 2 ); 
+const geometry3 = new THREE.BoxGeometry( 3, 7, 2.5 ); 
 const material3 = new THREE.MeshBasicMaterial( {color: 0x00fff0} ); 
 const LeftArm = new THREE.Mesh( geometry3, material3 ); 
 LeftArm.position.x = -7;
