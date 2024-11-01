8286e74945cb34bfaa672a5597c31cb4e46fcd24
EthanBarrett <145779498+EthanBarrett@users.noreply.github.com>
2024-11-01 16:49:55 +0000

    upadtes

--- Code/WebGameProject/threesample.js
+++ Code/WebGameProject/threesample.js
@@ -4,9 +4,12 @@ import { OrbitControls } from 'https://unpkg.com/three@0.157.0/examples/jsm/cont
 
 import {GLTFLoader} from "https://unpkg.com/three@0.169.0/examples/jsm/loaders/GLTFLoader.js";
 
-let controls;
 
 
+let controls; //declare control parameter
+
+//animation mixer
+let mixer;
 
 //add control variables
 let upstate = false; //button up
@@ -14,7 +17,8 @@ let downstate = false; //button down
 let changed = false; //colour change
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -53,6 +57,8 @@ const material4 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
 const Legs = new THREE.Mesh(geometry4, material4 ); 
 Legs.position.y = -4
 
+
+
 //add to scene
 scene.add( Legs );
 scene.add(LeftArm);
@@ -70,6 +76,26 @@ group.add(LeftArm);
 group.add(Legs);
 scene.add(group);
 
+
+//pillar tower
+const geometry5 = new THREE.CylinderGeometry( 15, 15, 60, 96 ); 
+const material5 = new THREE.MeshBasicMaterial( {color: 0xff0000} ); 
+const tower = new THREE.Mesh( geometry5, material5 ); 
+tower.position.x = 34;
+tower.position.y = 10;
+tower.position.z = -34;
+
+scene.add( tower );
+
+
+//floor
+const geometry6 = new THREE.PlaneGeometry( 200, 100 );
+const material6 = new THREE.MeshBasicMaterial( {color: 336699, side: THREE.DoubleSide} );
+const floor = new THREE.Mesh( geometry6, material6 );
+floor.rotation.x = 1.6;
+floor.position.y = -16;
+scene.add( floor );
+
 //////////////////////////////////////////////////////////////////
 //change camera position
 camera.position.z = 50;
@@ -86,27 +112,20 @@ const plane = new THREE.Mesh( geometry7, material7 );
 plane.position.x = x;
 plane.position.y = -15;
 plane.position.x = -Math.PI/2;
-plane.rotation.x = 4.5;
+plane.rotation.x = 1.6;
 scene.add(plane);
 
 }
-
+//texture of plane
 const texture = new THREE.TextureLoader().load("resources/img/goldpattern.png");
 const materialAspectfloor = {
 	map:texture,
 	side: THREE.DoubleSide,
 	transparent:true
 }
-addPlane(0, -3.6, 30, 30, materialAspectfloor);
+addPlane(0, -3.6, 60, 60, materialAspectfloor);
 
 //////////////////////////////////////////////////
-
-//add player
-const player = new THREE.Mesh(geometry3, material3);
-scene.add(player);
-
-///////////////////////////////////////////////////////
-
 
 
 // a function that will be called every time the window gets resized.
@@ -133,7 +152,7 @@ const createskybox = ()=>{
 	let bgMesh;
 	const loader = new THREE.TextureLoader();
 	loader.load("resources/img/galaxy.jpg", function(texture){
-		const sphereGeometry = new THREE.SphereGeometry(100, 60, 40);
+		const sphereGeometry = new THREE.SphereGeometry(1000, 60, 40);
 		const sphereMaterial = new THREE.MeshBasicMaterial({
 			map: texture,
 			side: THREE.DoubleSide
@@ -178,21 +197,28 @@ document.getElementById("downbutton").addEventListener("click", movedown);
 function animate() {
 
 	Head.rotation.x += 0.01;
-	group.rotation.y += 0.01;
-	RightArm.rotation.y =0;
-
+	
 	if(upstate)
 	{
-		player.position.y +=0.02;
-
+		group.position.z -=0.05;
+		
 	} 
 	else if (downstate)
 	{
-		player.position.y -= 0.02;
+		group.position.y -= 0.05;
 	}
 
-//change color
+	if (group.position.y > 3)
+	{
+		
+		upstate = false;
+	}
 
+	if (group.position.y < -3)
+	{
+		
+		downstate = false;
+	}
 
 	renderer.render( scene, camera );
 
